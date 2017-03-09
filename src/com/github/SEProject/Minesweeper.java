9bd21014f2c0bc2aa326fe5573d13defcd7f87ed
Austin Toot <austin@astronomer.io>
2017-03-09 15:06:25 -0500

    Revert "Added Coordinate tests, also changed coordinate constructor to public"
    This reverts commit d2389ffebfffa311593aa8024e5b817006e1fc27.

---
+++ src/com/github/SEProject/Minesweeper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.SEProject;
+import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class Minesweeper extends JPanel{
+
+public static void main(String args[]){;
+        Window w = new Window(10,10,10);
+        //Map gameMap = new Map(rows, columns, 10);
+        
+    }
+    
+}
