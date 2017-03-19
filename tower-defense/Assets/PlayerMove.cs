54d8c2adbae5029bd2cfd71bae70b1d3bbc0fb66
adt40 <adt@case.edu>
2017-03-19 19:44:36 -0400

    player can move with wasd and will aim towards mouse

---
+++ tower-defense/Assets/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float Speed = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+		movex = Input.GetAxis ("Horizontal");
+		movey = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = new Vector2 (movex * Speed, movey * Speed);
+	}
+}
