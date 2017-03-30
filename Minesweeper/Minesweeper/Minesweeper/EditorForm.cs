a9c98c0b3f7bfd81c5c9363825c810758c168b36
Joseph <jab363@case.edu>
2017-03-30 14:39:31 -0400

    in-class debugging
    Signed-off-by: Joseph <jab363@case.edu>

--- Minesweeper/Minesweeper/Minesweeper/EditorForm.cs
+++ Minesweeper/Minesweeper/Minesweeper/EditorForm.cs
@@ -62,14 +62,7 @@ namespace Minesweeper
                     if (buttons[x, y].Equals(button))
                     {
                         Coordinate c = new Coordinate(x, y);
-                        if (map.squares[c].isBomb)
-                        {
-                            map.squares[c].isBomb = false;
-                        } else
-                        {
-                            map.squares[c].isBomb = true;
-                        }
-                        
+                        map.squares[c].isBomb = !map.squares[c].isBomb;                        
                         updateAdj();
 
 
@@ -125,7 +118,7 @@ namespace Minesweeper
                 {
                     Coordinate c = new Coordinate(x, y);
                     Square square = map.squares[c];
-                    if (square.isBomb)
+                    if (!square.isBomb)
                     {
                         int adj = square.numAdjBombs;
                         if (adj != 0)
