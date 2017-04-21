8bd8d67f93fa5627f8bcf2fb2bc781193fd9aa16
arcdawn <supersmash233@gmail.com>
2017-04-20 23:28:08 -0400

    Added leaderboard
    TODO -
    Create displayable for leaderboard

--- Minesweeper/Minesweeper/Minesweeper/WinForm.Designer.cs
+++ Minesweeper/Minesweeper/Minesweeper/WinForm.Designer.cs
@@ -30,37 +30,69 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(16, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(325, 17);
+            this.label1.Size = new System.Drawing.Size(257, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = " AW HECKN\' YES U DID IT M80 I KNEW U COULD";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(115, 77);
-            this.button1.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.button1.Location = new System.Drawing.Point(19, 63);
+            this.button1.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 60);
+            this.button1.Size = new System.Drawing.Size(100, 49);
             this.button1.TabIndex = 2;
             this.button1.Text = "BASK IN THE GLORY OF THE MAIN MENU";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Upload Score";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name";
+            // 
             // WinForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 181);
+            this.ClientSize = new System.Drawing.Size(284, 147);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "WinForm";
             this.Text = "WinForm";
             this.Load += new System.EventHandler(this.WinForm_Load);
@@ -73,5 +105,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Minesweeper/Minesweeper/Minesweeper/WinForm.cs
+++ Minesweeper/Minesweeper/Minesweeper/WinForm.cs
@@ -7,28 +7,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Amazon.S3.Model;
+using System.Diagnostics;
+using System.IO;
 
 namespace Minesweeper
 {
     public partial class WinForm : Form
     {
+        TransferUtility utility;
         GameForm game;
         public WinForm(GameForm game) {
             this.game = game;
             InitializeComponent();
         }
-
+        public WinForm()
+        {
+            InitializeComponent();
+            //for unit testing
+        }
         private void WinForm_Load(object sender, EventArgs e)
         {
 
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
             MainForm main = new MainForm();
             main.Show();
             game.Hide();
             this.Hide();
+        }
+        private void downloader(String filename)
+        {
+            //download file
+            
+            TransferUtilityDownloadRequest request = new TransferUtilityDownloadRequest();
+            request.BucketName = "eecs393highscore";
+            
+            request.FilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Minesweeper\\" + filename + ".score";
+            request.Key = filename + ".score";
+            utility.Download(request);
+        }
+        private void uploader(String filename)
+        {
+            TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
+            request.BucketName = "eecs393highscore";
+            request.FilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Minesweeper\\" + filename + ".score";
+            utility.Upload(request);
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //putting a highScore file online
+            //download latest file
+            String filename = "highScore";
+            downloader(filename);
+            String USR = textBox1.Text+" ";
+            Debug.Print(Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Minesweeper").ToString());
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Minesweeper"))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Minesweeper");
+            }
+            StreamWriter file = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Minesweeper\\" + filename + ".score");
+            File.AppendAllText(filename + ".score", USR);
+
+            //uploading file
+            uploader(filename);
+
+            
         }
     }
 }
