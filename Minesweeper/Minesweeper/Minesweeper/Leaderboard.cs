9ac065e00eb9bf036c475b44d516b16fedf31946
arcdawn <supersmash233@gmail.com>
2017-04-22 14:57:02 -0400

    Added a leaderboard thing that will be displayed (needs implementation) after uploading
    Need to fix implementation of high scores as metadata in .map files

---
+++ Minesweeper/Minesweeper/Minesweeper/Leaderboard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Minesweeper
+
+{
+    public partial class Leaderboard : Form
+    {
+        public Leaderboard()
+        {
+            
+            String file = "highScore";
+            displayLeaderboard(file);
+            InitializeComponent();
+        }
+        public static void displayLeaderboard(String filename)
+        {
+
+            
+        }
+        
+       
+    }
+}
